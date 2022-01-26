2a6ab53cce704549fce15227f0bcb14c06d39c6d
Tadhg N <tadhgnolan@gmail.com>
2022-01-26 15:53:10 +0000

    Add Category & Post classes to models.py

--- readit/models.py
+++ readit/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+
+    category = models.ForeignKey(Category, on_delete=SET_NULL)
+
+    def __str__(self):
+        return self.title

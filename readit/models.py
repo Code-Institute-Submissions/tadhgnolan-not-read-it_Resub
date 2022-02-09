5fd93572e5d5bf1ef2d1baef893bee0b7f7c51ee
Tadhg N <tadhgnolan@gmail.com>
2022-02-09 12:21:54 +0000

    Add preview html

--- readit/models.py
+++ readit/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=25, unique=True)
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), 
-                                  empty_label="(Nothing)")
 
     def __str__(self):
         return self.name
+        
+    class Meta:
+        verbose_name_plural = "Categories"  
+
 
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=200)
-
-    category = models.ForeignKey(Category, on_delete=SET_NULL)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title

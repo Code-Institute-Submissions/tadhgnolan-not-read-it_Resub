b0849c5028d68a789f9fbaa7cce9f33dbaa24fb5
Tadhg N <tadhgnolan@gmail.com>
2022-07-21 21:10:06 +0000

    Correct error in file path in base.html

--- notreadit/settings.py
+++ notreadit/settings.py
@@ -16,7 +16,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "not-read-it.herokuapp.com"]
 X_FRAME_OPTIONS = "SAMEORIGIN"
